006e8978ac4c72e73d0073d7548af262d846d3a4
Anja Berens <pharaun666@gmail.com>
2012-07-20 00:26:47 -0700

    Migrated css over to SASS, cleaned up the CSS/SCSS

---
+++ lib/constants.rb
@@ -0,0 +1,19 @@
+require 'sass'
+
+module Sass::Script::Functions
+  # returns the opposite position of a side or corner.
+  def opposite_position(position)
+    opposite = position.value.split(/ +/).map do |pos|
+      case pos
+      when "top" then "bottom"
+      when "bottom" then "top"
+      when "left" then "right"
+      when "right" then "left"
+      when "center" then "center"
+      else
+        raise Sass::SyntaxError, "Cannot determine the opposite of #{pos}"
+      end
+    end
+    Sass::Script::String.new(opposite.join(" "), position.type)
+  end
+end
